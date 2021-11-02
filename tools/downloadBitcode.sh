5474fec1239fc56659d08e60d5d179a530b14133
Ed Guy <edguy@eguy.org>
2021-11-02 15:15:13 -0400

    add local cache to tools/downloadBitcode.sh

--- tools/downloadBitcode.sh
+++ tools/downloadBitcode.sh
@@ -10,6 +10,9 @@ PACKAGE_VERSION=$(cat "${THIS_DIR}/../package.json" \
   | sed 's/[",]//g' \
   | tr -d '[[:space:]]')
 WEBRTC_DL="https://github.com/react-native-webrtc/react-native-webrtc/releases/download/${PACKAGE_VERSION}/WebRTC.tar.xz"
+CACHED_DIR=~/.cache
+echo ${CACHED_DIR}
+CACHED_FILE="${CACHED_DIR}/WebRTC-${PACKAGE_VERSION}.tar.xz"
 
 
 pushd "${THIS_DIR}/../apple"
@@ -20,7 +23,14 @@ rm -rf WebRTC.xcframework WebRTC.dSYMs
 # Download
 echo "Downloading files..."
 echo $PACKAGE_VERSION
-curl -L -s ${WEBRTC_DL} | tar Jxf -
+if [ -d "$CACHED_DIR" ]
+then
+  echo "Using Cache from ${CACHED_FILE}"
+  [ -e "${CACHED_FILE}" ] || ( echo "Priming Cache..." && curl -L -s ${WEBRTC_DL} -o "${CACHED_FILE}")
+  tar Jxf "${CACHED_FILE}"
+else
+  echo curl -L -s ${WEBRTC_DL} | tar Jxf -
+fi
 echo "Done!"
 
 popd
